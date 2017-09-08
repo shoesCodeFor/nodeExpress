57693d87739d6e3e20a5595903a72afeb5bb6f62
Schuyler Ankele <schuyler.ankele@gmail.com>
2017-09-08 00:36:16 -0600

    Basic Routing: Home and About.  Getting ready for middleware 23:00.  Ignoring errors.  App is working.

--- nodeExpress.js
+++ nodeExpress.js
@@ -4,12 +4,30 @@ var app = express();
 
 app.disable('x-powered-by');
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// More Imports here
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+/*
 app.get('/', function (req, res){
     res.send('<h1>Express is Working!</h1>');
 });
 
+
+*/
+app.get('/', function(req, res){
+    res.render('home');
+});
+app.get('/about', function(req, res){
+    res.render('about');
+});
+
 app.listen(app.get('port'), function(){
-    console.log('Press CTRL-C to quit');
+    console.log('Express started on http://localhost:' + app.get('port') + ' Press CTRL-C to exit');
 });
